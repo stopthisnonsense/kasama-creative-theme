4227d1d96acbcc0d2a04b630d5e3fdae728789c3
Amarvin <anthony@glacial.com>
2021-07-29 10:45:04 -0400

    add focus, delineate footer links

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 
     <footer class="footer grid padding-sm padding-y">
       <div class="grid__column grid__column--12 grid__column--3--lg">
-        <a href="<?= home_url(); ?>" class="header__link color-lightest title type-center"><?php bloginfo( 'site_title' ); ?></a>
+        <a href="<?= home_url(); ?>" class="footer__link color-lightest title type-center"><?php bloginfo( 'site_title' ); ?></a>
       </div>
       <div class="grid__column grid__column--12 grid__column--9--lg">
         <?php
